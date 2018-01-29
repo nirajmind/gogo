b591a1ae4c123287f5a1cdc3aaae2fbb3a9d3891
AWS CodeStar <noreply-awscodestar@amazon.com>
2018-01-29 14:06:19 +0000

    Initial commit made by AWS CodeStar during project creation.

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"io/ioutil"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	index, err := ioutil.ReadFile("public/index.html")
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       string(index),
+		Headers: map[string]string{
+			"Content-Type": "text/html",
+		},
+	}, nil
+
+}
+
+func main() {
+	lambda.Start(handler)
+}
